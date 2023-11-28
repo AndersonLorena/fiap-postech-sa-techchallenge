2643dd4b47edda6de8f4f652ccd11825d0e6186d
Anderson Lorena <anderson_datadyne@hotmail.com>
2023-11-28 00:24:40 -0300

    created the two first abstraction controllers.

--- src/AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi/Controllers/IngredientController.cs
+++ src/AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi/Controllers/IngredientController.cs
@@ -1,6 +1,29 @@
-﻿namespace AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers
+﻿using AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers.Base;
+using AL.Fiap.PosTech.TechChallenge.Application.CommandHandlers;
+using AL.Fiap.PosTech.TechChallenge.Domain.Entities;
+using AL.Fiap.PosTech.TechChallenge.Ports.Queries;
+using AL.Fiap.PosTech.TechChallenge.Ports.Queries.Dtos;
+using AutoMapper;
+using MediatR;
+
+namespace AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers
 {
-    public class IngredientController
+    public sealed class IngredientController : CrudBaseController<
+        CreateIngredientCommand,
+        IngredientDto,
+        UpdateIngredientCommand,
+        DeleteIngredientCommand,
+        IngredientEntity>
     {
+        public IngredientController(
+            ISender mediator,
+            IQueryRepository<IngredientEntity> queryRepository,
+            IMapper mapper)
+            : base(
+                  mediator,
+                  queryRepository,
+                  mapper)
+        {
+        }
     }
 }
--- src/AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi/Controllers/OrderController.cs
+++ src/AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi/Controllers/OrderController.cs
@@ -1,6 +1,32 @@
-﻿namespace AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers
+﻿using AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers.Base;
+using AL.Fiap.PosTech.TechChallenge.Application.CommandHandlers;
+using AL.Fiap.PosTech.TechChallenge.Domain.Entities;
+using AL.Fiap.PosTech.TechChallenge.Ports.Queries;
+using AL.Fiap.PosTech.TechChallenge.Ports.Queries.Dtos;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AL.Fiap.PosTech.TechChallenge.Adapters.Entrypoints.WebApi.Controllers
 {
-    public class OrderController
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class OrderController : CrudBaseController<
+        CreateOrderCommand,
+        OrderDto,
+        UpdateOrderCommand,
+        DeleteOrderCommand,
+        OrderEntity>
     {
+        public OrderController(
+            ISender mediator,
+            IQueryRepository<OrderEntity> queryRepository,
+            IMapper mapper)
+            : base(
+                  mediator,
+                  queryRepository,
+                  mapper)
+        {
+        }
     }
 }
