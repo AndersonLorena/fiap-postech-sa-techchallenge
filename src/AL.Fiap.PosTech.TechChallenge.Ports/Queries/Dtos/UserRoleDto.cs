355cf7b8111173409dc61477819bdf2cb9204161
Anderson Lorena <anderson_datadyne@hotmail.com>
2023-11-27 23:20:06 -0300

    created dtos.

---
+++ src/AL.Fiap.PosTech.TechChallenge.Ports/Queries/Dtos/UserRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using AL.Fiap.PosTech.TechChallenge.Ports.Queries.Dtos.Base;
+
+namespace AL.Fiap.PosTech.TechChallenge.Ports.Queries.Dtos
+{
+    public partial class UserRoleDto : BaseDto
+    {
+        public int UserId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public virtual RoleDto? Role { get; set; }
+    }
+}
